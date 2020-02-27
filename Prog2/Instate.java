0badf549efc4080a757e7985ca6205747a3ab323
Henry <yz776@scarletmail.rutgers.edu>
2020-02-27 16:47:30 -0500

    added student and instate

--- Prog2/Instate.java
+++ Prog2/Instate.java
@@ -1,3 +1,30 @@
 public class Instate extends Student {
+    public static final int IN_STATE_CREDIT = 433;
     private int funds;
+    public Instate(String fname, String lname, int credit, int funds){
+        super(fname, lname, credit);
+        this.funds = funds;
+    }
+    public int tuitionDue(){
+        int fee = 0;
+        if (credit < PART_TIME_CUTOFF){
+            fee = PART_TIME_FEE;
+        }
+        else {
+            fee = FULL_TIME_FEE;
+        }
+        int total = 0;
+        if (credit > CREDIT_CUTOFF){
+            total = CREDIT_CUTOFF * IN_STATE_CREDIT;
+        }
+        else {
+            total = this.credit * IN_STATE_CREDIT;
+        }
+        total += fee;
+        return total;
+    }
+    public String toString() {
+        return super.toString() + " " + funds;
+    }
+
 }
--- Prog2/Student.java
+++ Prog2/Student.java
@@ -1,16 +1,30 @@
 public abstract class Student implements Comparable {
+    public static final int PART_TIME_FEE = 846;
+    public static final int FULL_TIME_FEE = 1441;
+    public static final int PART_TIME_CUTOFF = 12;
+    public static final int CREDIT_CUTOFF = 15;
     private String fname;
     private String lname;
     protected int credit;
-    public Student(String fname, String lname, int credit) {} //constructor
+    public Student(String fname, String lname, int credit) {
+        this.fname = fname;
+        this.lname = lname;
+        this.credit = credit;
+    } //constructor
     //must implement compareTo method because Student class implements the Comparable Interface
 //return 0 if fname and lname of the two students are equal,
 //return -1 if this fname and lname is < obj’s, return 1 if this fname and lname is > obj’s
 //Hint: use the .equals and compareToIgnoreCase methods of the String class to compare fname
 //and lname; names are not case-sensitive
-    public int compareTo(Object obj){}
+    public int compareTo(Object obj){
+        String thisName = this.fname + this.lname;
+        String objName = ((Student) obj).fname + ((Student) obj).lname;
+        return thisName.compareToIgnoreCase(objName);
+    }
     //return a string with fname, lname and credit hours; subclasses will be using this method.
-    public String toString() {}
+    public String toString() {
+        return this.fname + " " + this.lname + " " + this.credit;
+    }
     //compute the tuition due; concrete implementation is required in the subclasses.
-    public abstract int tuitionDue() {}
+    public abstract int tuitionDue();
 }
