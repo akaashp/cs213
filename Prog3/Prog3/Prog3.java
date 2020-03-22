e957c83dac5dace763491d9b35430ffbccb48f90
akaashpatel <patelakaash75@gmail.com>
2020-03-22 18:52:21 -0400

    new files

--- Prog3/Prog3/Prog3.java
+++ Prog3/Prog3/Prog3.java
@@ -2,9 +2,8 @@
  * Driver class that calls run from TuitionManager
  @author  Akaash Patel, Yanheng Zhang
  */
-public class Prog2 {
-    public static void main(String [] args)
-    {
+public class Prog3 {
+    public static void main(String [] args){
         new TuitionManager().run();
     }
 }
