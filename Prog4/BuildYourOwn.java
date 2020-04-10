a2ce30d8e06857646e27bf1fcaaa79be186b46fd
Henry <yz776@scarletmail.rutgers.edu>
2020-04-10 15:38:47 -0400

    added proj4 back end files

---
+++ Prog4/BuildYourOwn.java
@@ -0,0 +1,50 @@
+/**
+ * This class defines the properties and methods of BuildYourOwn, a subclass of Pizza. BuildYourOwn implements
+ * pizzaPrice()to return price for pizza and overrides the toString method to return additional class-specific
+ * information
+ *
+ * @author  Akaash Patel, Yanheng Zhang
+ */
+
+import java.util.ArrayList;
+
+public class BuildYourOwn extends Pizza {
+    public static final int SML_BYO_PRICE = 5;
+    public static final int MED_BYO_PRICE = 7;
+    public static final int LRG_BYO_PRICE = 9;
+    public static final int PRICE_PER_TOPPING = 2;
+
+    /**
+     * Constructor for BuildYourOwn pizza
+     * @param size Size of Hawaiian pizza
+     */
+    public BuildYourOwn(String size, ArrayList<String> toppings){
+        super("byo", size, toppings);
+    }
+
+    /**
+     * Returns price of BuildYourOwn pizza
+     * @return Int, using one of the three static variables and price per topping multiplied by size of topping list
+     */
+    public int pizzaPrice(){
+        if (size.equals("small")){
+            return SML_BYO_PRICE + toppings.size() * PRICE_PER_TOPPING;
+        }
+        else if (size.equals("medium")){
+            return MED_BYO_PRICE + toppings.size() * PRICE_PER_TOPPING;
+        }
+        else {
+            return MED_BYO_PRICE + toppings.size() * PRICE_PER_TOPPING;
+        }
+    }
+
+    /**
+     * Overrides toString of parent class Pizza, calling the super method and adding price value to the end
+     * of the return value
+     * @return String representation of BuildYourOwn pizza
+     */
+    public String toString(){
+        return super.toString() + " " + this.pizzaPrice();
+    }
+
+}
---
+++ Prog4/Deluxe.java
@@ -0,0 +1,46 @@
+/**
+ * This class defines the properties and methods of Deluxe, a subclass of Pizza. Deluxe implements pizzaPrice()
+ * to return price for pizza and overrides the toString method to return additional class-specific information
+ *
+ * @author  Akaash Patel, Yanheng Zhang
+ */
+
+public class Deluxe extends Pizza {
+    public static final int SML_DELUXE_PRICE = 14;
+    public static final int MED_DELUXE_PRICE = 16;
+    public static final int LRG_DELUXE_PRICE = 18;
+
+    /**
+     * Constructor for Deluxe pizza
+     * @param size Size of Deluxe pizza
+     */
+    public Deluxe(String size){
+        super("deluxe", size);
+    }
+
+    /**
+     * Returns price of Deluxe pizza
+     * @return Int, using one of the three static variables
+     */
+    public int pizzaPrice(){
+        if (size.equals("small")){
+            return SML_DELUXE_PRICE;
+        }
+        else if (size.equals("medium")){
+            return MED_DELUXE_PRICE;
+        }
+        else {
+            return LRG_DELUXE_PRICE;
+        }
+    }
+
+    /**
+     * Overrides toString of parent class Pizza, calling the super method and adding price value to the end
+     * of the return value
+     * @return String representation of Deluxe pizza
+     */
+    public String toString(){
+        return super.toString() + " " + this.pizzaPrice();
+    }
+
+}
